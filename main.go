9a67cd87d54bf33ba125f32f5da9c1b3497e7f5b
Nikhil Singh <nikhil.singh@mongodb.com>
2019-10-11 14:13:08 +0530

    my first function with loop, condition

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Playground")
+	foo()
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+	bar()
+}
+
+func foo() {
+	fmt.Println("I'm in foo.")
+}
+
+func bar() {
+	fmt.Println("and then we exited")
+}
