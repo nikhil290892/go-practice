dbf80206f0bc5b746cfca88df682e28540ae7892
Nikhil Singh <nikhil.singh@mongodb.com>
2019-10-12 13:31:36 +0530

    Use string as a data type in global variable

--- keyword.go
+++ keyword.go
@@ -5,11 +5,14 @@ import (
 )
 
 var y = 39
+var z string
 
 func keyword() {
 	//short declaration operator
 	//Declare a variable and assign a value of a certain type at the same time
 	x := 54
+	z = "Nikhil"
 	fmt.Println("Value of x", x)
 	fmt.Println("Value of global variable y", y)
+	fmt.Println(z, "is the programmer for this")
 }
