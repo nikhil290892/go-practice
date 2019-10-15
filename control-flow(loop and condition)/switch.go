444acaf1fbced447fca322ccee174f25ee308ee5
Nikhil Singh <nikhil.singh@mongodb.com>
2019-10-15 20:55:32 +0530

    switch case

---
+++ control-flow(loop and condition)/switch.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func switchcase() {
+	switch {
+	case false:
+
+		fmt.Println("Nikhil")
+
+	case (2 == 2):
+
+		fmt.Println("Hello Nikhil")
+
+	case (4 == 4):
+
+		fmt.Println("Howdy Nikhil")
+
+	}
+}
